97168da85838e0f0f3968bd1265ba65cf0daacc5
Dillon Sandhu <dillonmsandhu@gmail.com>
2026-06-23 14:11:10 -0400

    random policy/td_exact

--- random_policy/td_exact.py
+++ random_policy/td_exact.py
@@ -23,31 +23,33 @@ def make_train(config):
     evaluator = helpers.initialize_evaluator(config, env, env_params)
     obs_shape = env.observation_space(env_params).shape
     n_actions = env.action_space(env_params).n
+    S = evaluator.obs_stack
+    n_states = len(evaluator.obs_stack) # also 
     
     # Policy to be evaluated
-    def get_random_policy_matrix(num_states: int, num_actions: int) -> jax.Array:
+    def get_random_policy_matrix(obs_stack=None) -> jax.Array:
         """
-        Produces a uniform random policy matrix PI of shape (num_total_states, num_actions).
+        Produces a uniform random policy matrix PI of shape (num_total_states, n_actions).
         
         Args:
-            num_states: The number of active states in the environment.
-            num_actions: The total number of available actions.
+            n_states: The number of active states in the environment.
+            n_actions: The total number of available actions.
         """
         # 1. Create uniform distribution for active states (1/N probability per action)
-        pi_active = jnp.ones((num_states, num_actions)) / num_actions
+        pi_active = jnp.ones((n_states, n_actions)) / n_actions
         
         # 2. Create uniform distribution for the single terminal state
-        pi_terminal = jnp.ones((1, num_actions)) / num_actions
+        pi_terminal = jnp.ones((1, n_actions)) / n_actions
         
         # 3. Stack them to match your evaluator's S+1 state requirement
         pi = jnp.vstack([pi_active, pi_terminal])
         
         return pi
     
-    Pi = get_random_policy_matrix(evaluator.num_states, evaluator.num_actions)
+    Pi = get_random_policy_matrix()
     
     # Get the Markov Chain
-    S = evaluator.obs_stack
+    
     P = evaluator.P # 3d tensor S x A x S'
     P_π = jnp.einsum("sa,sam->sm", Pi, P)
     R_π_s = jnp.einsum("sa,sa->s", Pi, evaluator.R)
@@ -98,7 +100,7 @@ def make_train(config):
             train_state, loss = jax.lax.scan(td_step, train_state, None, config["NUM_EPOCHS"])
             # 2. Get value metrics and logging
             metric = bellman_error.value_metrics(
-                evaluator, network, train_state.params, random_policy=False, target_policy_fn=get_policy
+                evaluator, network, train_state.params, random_policy=True, 
             )
             metric.update({"total_loss": loss.mean(), "value_loss": loss.mean()})
             runner_state = (train_state, idx + 1)
